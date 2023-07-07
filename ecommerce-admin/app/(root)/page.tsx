e55b8ecb70172189bfd5b75c488b53494110c8b8
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-07-07 16:40:31 +0200

    Clerk Authentication (Admin)

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import {Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-    return (
-      
+const SetupPage = () => {
+  return (
+    <>
       <div className="p-4">
-        <Button size="default" className="bg-orange-500 text-black" variant="destructive" >Click me</Button>
+        <UserButton afterSignOutUrl="/"/>
       </div>
-    )
-  }
-  
+    </>
+  );
+};
+
+export default SetupPage;
