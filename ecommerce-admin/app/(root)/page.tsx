2a1f37cc2189629f1e63025ea7a54d714250e54e
khoubaib-sudo <83957252+khoubaib-sudo@users.noreply.github.com>
2023-07-07 14:23:24 +0200

    Environment setup (Admin)

---
+++ ecommerce-admin/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import {Button } from "@/components/ui/button"
+
+export default function Home() {
+    return (
+      
+      <div className="p-4">
+        <Button size="default" className="bg-orange-500 text-black" variant="destructive" >Click me</Button>
+      </div>
+    )
+  }
+  
